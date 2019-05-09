de3440b49f4b5e43f000b3648ebdd17fd5708e2c
hsycamp <hsycamp@gmail.com>
2019-05-09 16:28:44 +0900

    시저암호 문제풀이

---
+++ programmers/Level_1/CaesarCipher.py
@@ -0,0 +1,37 @@
+'''
+프로그래머스
+시저 암호
+https://programmers.co.kr/learn/courses/30/lessons/12926
+
+문제 설명
+어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 AB는 1만큼 밀면 BC가 되고, 3만큼 밀면 DE가 됩니다. z는 1만큼 밀면 a가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
+
+제한 조건
+공백은 아무리 밀어도 공백입니다.
+s는 알파벳 소문자, 대문자, 공백으로만 이루어져 있습니다.
+s의 길이는 8000이하입니다.
+n은 1 이상, 25이하인 자연수입니다.
+'''
+
+
+def solution(s, n):
+    # A = 65, Z = 90
+    # a= 97, z = 122
+    lst = list(s)
+    answer = ''
+    for i in lst:
+        if 64 < ord(i) < 91:
+            if ord(i) + n > 90:
+                k = ord(i)+n - 90+64
+                answer += chr(k)
+            else:
+                answer += chr(ord(i)+n)
+        elif 96 < ord(i) < 123:
+            if ord(i) + n > 122:
+                k = ord(i)+n - 122+96
+                answer += chr(k)
+            else:
+                answer += chr(ord(i)+n)
+        else:
+            answer += ' '
+    return answer
