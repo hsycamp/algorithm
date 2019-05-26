004723f26ac3e68b1dbfc3cf013f7e7e48fcb551
hsycamp <hsycamp@gmail.com>
2019-05-26 23:39:29 +0900

    MaxCounters 문제풀이
    풀이 추가

--- codility/MaxCounters.py
+++ codility/MaxCounters.py
@@ -5,7 +5,7 @@ Calculate the values of counters after applying all alternating operations: incr
 
 
 # 시간초과
-def solution(N, A):
+def solution1(N, A):
     lst = [0] * N
     for i in range(len(A)):
         for j in range(N):
@@ -14,3 +14,26 @@ def solution(N, A):
             if A[i] == N+1:
                 lst = [max(lst)] * N
     return lst
+
+
+def solution2(N, A):
+    lst = [0]*N
+    count = 0
+    maxCount = 0
+    for i in range(len(A)):
+        if A[i] == N+1:
+            maxCount = count
+        else:
+            if lst[A[i]-1] < maxCount:
+                lst[A[i]-1] = maxCount
+            lst[A[i]-1] += 1
+            if lst[A[i]-1] > count:
+                count = lst[A[i]-1]
+    bridge_the_gap(lst, maxCount)
+    return lst
+
+
+def bridge_the_gap(lst, maxCount):
+    for i in range(len(lst)):
+        if lst[i] < maxCount:
+            lst[i] = maxCount
