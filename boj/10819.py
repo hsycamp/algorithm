770ed5d9822fc0155eca10ce5b74135eeae49d25
hsycamp <hsycamp@gmail.com>
2019-07-23 16:26:00 +0900

    차이를 최대로 문제풀이
    문제 출처 수정

--- boj/10819.py
+++ boj/10819.py
@@ -1,7 +1,7 @@
 '''
 BOJ
 10819번 차이를 최대로
-https://www.acmicpc.net/problem/11399
+https://www.acmicpc.net/problem/10819
 
 문제
 N개의 정수로 이루어진 배열 A가 주어진다. 이때, 배열에 들어있는 정수의 순서를 적절히 바꿔서 다음 식의 최댓값을 구하는 프로그램을 작성하시오.
