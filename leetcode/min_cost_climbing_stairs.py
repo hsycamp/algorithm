5f9d8aa4c23e9f7b5695dc280e6ca439e0b95768
hsycamp <hsycamp@gmail.com>
2019-08-27 12:19:30 +0900

    Min Cost Climbing Stairs 문제풀이

---
+++ leetcode/min_cost_climbing_stairs.py
@@ -0,0 +1,29 @@
+'''
+LeetCode Min Cost Climbing Stairs 문제
+https://leetcode.com/problems/min-cost-climbing-stairs/
+
+746. Min Cost Climbing Stairs
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+Example 1:
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+Example 2:
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+Output: 6
+Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+Note:
+cost will have a length in the range [2, 1000].
+Every cost[i] will be an integer in the range [0, 999].
+'''
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        finalCost = [cost[0],cost[1]]
+        for i in range(2,len(cost)):
+            finalCost.append(cost[i]+min(finalCost[i-2],finalCost[i-1]))
+        return min(finalCost[-2],finalCost[-1])
